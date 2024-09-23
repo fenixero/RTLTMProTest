66e66012f8c69c4ee54df623d1368099b6fa7066
WenTao <wentao0107@126.com>
2024-09-23 17:09:20 +0800

    add copyright notice

--- UPMPackage/Scripts/Runtime/FastStringBuilder.cs
+++ UPMPackage/Scripts/Runtime/FastStringBuilder.cs
@@ -1,3 +1,6 @@
+// The SeedV Lab (Beijing SeedV Technology Co., Ltd.) modifications: Copyright 2024 The SeedV Lab
+// (Beijing SeedV Technology Co., Ltd.) All Rights Reserved. The modifications in this file are the intellectual property of the SeedV Lab.
+
 using System;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
