035f9b97375831be4da65fe9f2b3e2e2bea46e13
WenTao <wentao0107@126.com>
2024-09-26 14:50:12 +0800

    fix code style

--- UPMPackage/Scripts/Runtime/TagValidater.cs
+++ UPMPackage/Scripts/Runtime/TagValidater.cs
@@ -13,29 +13,29 @@ namespace RTLTMPro {
 
     public RTLTextMeshPro() {
       _unicodeCharType = typeof(TMP_Text).GetNestedType("UnicodeChar",
-          BindingFlags.NonPublic | BindingFlags.Instance);
+        BindingFlags.NonPublic | BindingFlags.Instance);
       _unicodeField = _unicodeCharType.GetField("unicode");
       _stringIndexField = _unicodeCharType.GetField("stringIndex");
       _lengthField = _unicodeCharType.GetField("length");
       _methodValidateHtmlTag = typeof(TextMeshProUGUI).GetMethod("ValidateHtmlTag",
-          BindingFlags.NonPublic | BindingFlags.Instance);
+        BindingFlags.NonPublic | BindingFlags.Instance);
     }
 
     public List<(int, int)> FindTags(string input) {
-      var tags = new List<(int,int)>();
+      var tags = new List<(int, int)>();
       for (int i = 0; i < input.Length; i++) {
         char ch = input[i];
-        if (ch == 60)  // '<'
-        {
+        if (ch == 60) {
           // Check if Tag is valid. If valid, skip to the end of the validated tag.
-          if (ValidateTag(input, i + 1, out int endTagIndex))
-          {
-            tags.Add((i,endTagIndex));
+          if (ValidateTag(input, i + 1, out int endTagIndex)) {
+            tags.Add((i, endTagIndex));
           }
         }
       }
+
       return tags;
     }
+
     private bool ValidateTag(string input, int startIndex, out int endIndex) {
       if (_methodValidateHtmlTag != null) {
         var unicodeChars = Array.CreateInstance(_unicodeCharType, input.Length);
@@ -47,6 +47,7 @@ namespace RTLTMPro {
           _lengthField.SetValue(unicodeChar, 1);
           unicodeChars.SetValue(unicodeChar, i);
         }
+
         object[] parameters = { unicodeChars, startIndex, null };
         bool result = (bool)_methodValidateHtmlTag.Invoke(this, parameters);
         endIndex = (int)parameters[2];
